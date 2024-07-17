a5a45a4c99b447295460e46626e2b0c73964cd18
leodipaula <leofernandes9@gnail.com>
2024-07-16 22:45:00 -0300

    Projeto base

---
+++ src/main/java/com/forumhub/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.forumhub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
